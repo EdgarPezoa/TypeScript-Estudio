f63bda186fe8065cc4f94e3c11539905ec02303b
Edgar Pezoa <pezoa.1@gmail.com>
2020-03-23 14:48:41 -0400

    Seecion 2 completada

--- 4 - Primeros pasos/app.js
+++
@@ -1,4 +0,0 @@
-console.log("Hola mundo");
-console.clear();
-console.warn("por favor cerrar la puerta");
-console.error("ERRRORRR");
---
+++ Seccion 2/Primeros pasos/app.ts
@@ -0,0 +1,89 @@
+// TIPOS DE CONSOLE LOG
+// console.log("Hola mundo");
+// console.clear();
+// console.warn("por favor cerrar la puerta");
+// console.error("ERRRORRR");
+
+// VARIABLE NUMEROS
+// var edad:number;
+// edad = 24;
+// console.log(edad);
+// var numeroIndefinido:number;
+// console.log(numeroIndefinido);
+
+// VARIABLE STRING
+// var nombre:string;
+// nombre = "Edgar";
+// nombre = nombre + " Pezoa";
+// console.log(nombre);|
+
+//VARIABLE BOOLEAN
+// var condicion:boolean = false;
+// var bandera:boolean = true;
+// var juan:string = "juan";
+// var juan2:string = "Juan";
+// var nombresIguales = (juan == juan2);
+// var aceptaDescuentios:boolean = (1+2 == 3);
+// var verdadero = aceptaDescuentios || nombresIguales;
+// var falso = aceptaDescuentios && nombresIguales;
+// var diferente = !(true);
+
+// OBJETOS
+// var persona = {
+//     nombre: "Edgar",
+//     apellido: "Pezoa",
+//     edad: 24,
+//     peso: 100
+// }
+// console.log(persona.nombre);
+
+// INTERFAS
+// interface Personas{
+//     nombre: string,
+//     apellido: string,
+//     edad: number,
+//     peso: number
+// }
+// var persona: Personas;
+// var persona1: Personas = {
+//     nombre: "Edgar",
+//     apellido: "Pezoa",
+//     edad: 24,
+//     peso: 100
+// }
+// console.log(persona.nombre);
+// console.log(persona1.nombre);
+
+// ANY
+// var canciones:any = "Musica";
+// canciones = 25;
+// console.log(canciones);
+
+// ARRAY
+// interface Personas{
+//     nombre: string,
+//     apellido: string,
+//     edad: number,
+//     peso: number
+// }
+// var listadoPersonas:Array<Personas> =[
+//     {
+//         nombre: "Edgar",
+//         apellido: "Pezoa",
+//         edad: 24,
+//         peso: 100
+//     },
+//     {
+//         nombre: "Jose",
+//         apellido: "Flores",
+//         edad: 23,
+//         peso: 120
+//     }
+// ];
+// var listado:Array<string> = ["juana", "maria", "ana"];
+// var listadoMixto:Array<any> = ["juana", 24, true];
+// console.log(listado);
+// console.log(listadoMixto);
+// console.log(listado[0]);
+// console.log(listadoPersonas);
+// console.log(listadoPersonas[0].nombre);
