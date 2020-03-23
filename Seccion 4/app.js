9b6ad048b86d6bfb7a08499e38e605c89753b9eb
Edgar Pezoa <pezoa.1@gmail.com>
2020-03-23 17:06:57 -0400

    seccion 4 finalizada

---
+++ Seccion 4/app.js
@@ -0,0 +1,31 @@
+//     // CICLO FOR
+// interface Usuario{
+//     nombre:string,
+//     apellido:string
+// }
+// var usuarios:Array<Usuario> = [
+//     {
+//         nombre: "edgar",
+//         apellido: "pezoa"
+//     },
+//     {
+//         nombre: "José",
+//         apellido: "Flores"
+//     },
+//     {
+//         nombre: "Enzo",
+//         apellido: "flores"
+//     }
+// ];
+//     // FOR NORMAL
+// for(let contador:number=0; contador< usuarios.length; contador++){
+//     console.log(usuarios[contador].nombre);
+// }
+//     // FOREACH
+// for(let usuario of usuarios){
+//     console.log(usuario);
+// }
+//     // FOR IN
+// for(let posicion in usuarios){
+//     console.log(usuarios[posicion]);
+// }
