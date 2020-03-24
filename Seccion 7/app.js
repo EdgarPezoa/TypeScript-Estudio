6074f72bac8511837688e73c8f6ca5af65173b4e
Edgar Pezoa <pezoa.1@gmail.com>
2020-03-23 21:08:13 -0400

    Seccion 7 Finalizada

---
+++ Seccion 7/app.js
@@ -0,0 +1,38 @@
+// Array push = integra un elemento al arreglo
+// Array splice = Elimina un elemento del arreglo (Indice de elemento, Cuantos desde aquel indice)    
+// var nombres:string[] = ["Edgar", "Jose", "Enzo"];
+// nombres.push("Daniel");
+// nombres.splice(1,1);
+// console.log(nombres);
+// interface Alumnos{
+//     nombre:string,
+//     edad:number
+// }
+// var alumnos:Alumnos[] = [];
+// var alumno:Alumnos={
+//     nombre: "Edgar",
+//     edad: 24
+// }
+// alumnos.push(alumno);
+// console.log(alumnos);
+// Funciones anonimas o funcioens de FLECHA
+// var nombres:string[] = ["Edgar", "Jose", "Enzo"];
+// nombres.forEach( (nombre) => console.log(nombre));
+// nombres.forEach( (nombre) => {
+//     if(nombre == "Edgar"){
+//         console.log("Encontré a edgar");
+//     }
+// });
+// interface Alumnos{
+//     id:number,
+//     nombre:string,
+//     edad:number
+// }
+// var alumnos:Array<any> = [];
+// alumnos.forEach( (alumno)=>{
+//     console.log(alumno.nombre+" "+alumno.edad);
+// });
+// var alumno = alumnos.find((alumno)=>{
+//     return alumno.id == 1;
+// });
+// console.log(alumno);
